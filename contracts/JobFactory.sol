eacd7258beeef3f6f8767b68ad9b0477ab5ae00f
Marvin Chan <marvinchan@Marvins-MacBook-Pro.local>
2022-04-02 12:52:17 -0400

    clean up

--- contracts/JobFactory.sol
+++ contracts/JobFactory.sol
@@ -6,6 +6,7 @@ import "./Job.sol";
 contract JobFactory {
 
     address[] public allJobs;
+    uint256 public value;
 
     event JobCreated(address job);
 
@@ -13,6 +14,7 @@ contract JobFactory {
         address newJobAddr =  address(new Job(_longitude, _latitude, _bounty, msg.sender, 10));
         allJobs.push(newJobAddr);
         (bool sent, bytes memory data) = payable(newJobAddr).call{value: msg.value}("");
+        value = msg.value;
         require(sent, "Eth not sent to new job");
         emit JobCreated(newJobAddr);
     }
